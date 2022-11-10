06acf45b661991e4ddaef984fa00233176dbdbbd
Jose M. Serra Jr <71533268+FPyC639@users.noreply.github.com>
2022-11-10 12:54:44 -0500

    Updated File

--- wget_file.ps1
+++ wget_file.ps1
@@ -1,7 +1,26 @@
-﻿$user = $args[0]
-$passwd = $args[1]
-$target = $args[2]
-$rename = $args[3]
+﻿param (
+        [Parameter(Mandatory = $false)]
+        [Alias('self')]
+        [AllowNull()]
+        ${user},
+        [Parameter(Mandatory = $false)]
+        [Alias('clearance')]
+        [AllowNull()]
+        ${passwd},
+        [Parameter(Mandatory = $true)]
+        [Alias('url')]
+        [AllowNull()]
+        ${target},
+        [Parameter(Mandatory = $true)]
+        [Alias('new_name')]
+        [AllowNull()]
+        ${rename})
 
 
-C:\Windows\System32\wget.exe "$target" --http-user="$user" --http-passwd="$passwd" -O "$rename"
+if ($user -ne $null){
+    C:\Windows\System32\wget.exe "target" --http-user="$user" --http-passwd="$passwd" -O "$rename"
+}
+else{
+    
+     C:\Windows\System32\wget.exe "$target" -O "$rename"
+}
